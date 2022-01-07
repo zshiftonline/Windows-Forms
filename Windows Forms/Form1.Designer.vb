e2fbcba0e47c8e6e4f2099943222f376bac31237
Dustin Boyden <zshiftonline@gmail.com>
2022-01-07 01:03:00 -0500

    added installer

--- Windows Forms/Form1.Designer.vb
+++ Windows Forms/Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.lblName = New System.Windows.Forms.Label()
         Me.txtName = New System.Windows.Forms.TextBox()
         Me.btnOK = New System.Windows.Forms.Button()
@@ -180,7 +181,9 @@ Partial Class Form1
         Me.Controls.Add(Me.btnOK)
         Me.Controls.Add(Me.txtName)
         Me.Controls.Add(Me.lblName)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
         Me.ResumeLayout(False)
         Me.PerformLayout()
